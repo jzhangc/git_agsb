d64bf9360508778d55c2c3e30a89bc54c778cfbc
Jing Zhang <jzhangc@gmail.com>
2021-10-23 17:59:27 -0400

    routine updates

---
+++ agsb.py
@@ -0,0 +1,15 @@
+# ------ libraries ------
+from selenium import webdriver
+import config
+
+
+# ------ G variables ------
+VERSION = '0.0.1'
+TGTURL = 'https://www.xbox.com/en-ca/configure/8WJ714N3RBTL?ranMID=36509&ranEAID=AKGBlS8SPlM&ranSiteID=AKGBlS8SPlM-GV4BRGqhq_Am72VVaANyMQ&epi=AKGBlS8SPlM-GV4BRGqhq_Am72VVaANyMQ&irgwc=1&OCID=AID2200057_aff_7814_1243925&tduid=%28ir__lkprzt0gd0kf6j3xn91u1x99af2xrfsfusvurmhu00%29%287814%29%281243925%29%28AKGBlS8SPlM-GV4BRGqhq_Am72VVaANyMQ%29%28%29&irclickid=_lkprzt0gd0kf6j3xn91u1x99af2xrfsfusvurmhu00'
+
+
+# ------ functions --------
+
+
+# ------ test realm ------
+print(config.NAME)
