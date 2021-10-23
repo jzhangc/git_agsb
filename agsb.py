5c642034a4493fe2b1178b708144e0519979755d
Jing Zhang <jzhangc@gmail.com>
2021-10-23 19:32:51 -0400

    routine  updates

--- agsb.py
+++ agsb.py
@@ -1,14 +1,42 @@
-# ------ libraries ------
-from selenium import webdriver
-import config
+'''
+Waterbox
+[ ] Construct a functional AGSB
+[ ] Get product links given product name
+[ ] Logging function
+[ ] Multiple browser support
+[ ] Multiple supplier support (Walmart, Bestbuy, Microsoft)
+[ ] Send me a text message when successful
+'''
 
+# ------ libraries ------
+import argparse
+import requests
+import requests_html
+import time
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+import config
 
 # ------ G variables ------
 VERSION = '0.0.1'
-TGTURL = 'https://www.xbox.com/en-ca/configure/8WJ714N3RBTL?ranMID=36509&ranEAID=AKGBlS8SPlM&ranSiteID=AKGBlS8SPlM-GV4BRGqhq_Am72VVaANyMQ&epi=AKGBlS8SPlM-GV4BRGqhq_Am72VVaANyMQ&irgwc=1&OCID=AID2200057_aff_7814_1243925&tduid=%28ir__lkprzt0gd0kf6j3xn91u1x99af2xrfsfusvurmhu00%29%287814%29%281243925%29%28AKGBlS8SPlM-GV4BRGqhq_Am72VVaANyMQ%29%28%29&irclickid=_lkprzt0gd0kf6j3xn91u1x99af2xrfsfusvurmhu00'
+TGTURL = config.TGTURL
 
 
 # ------ functions --------
+def main(*args, **kwargs):
+    return None
+
+
+# ------ main -------
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='AGSB arguments')
+    parser.add_argument('--name', required=True,
+                        help='Specify product name to find and purchase')
+    args = parser.parse_args()
+    main(target_product=args.name)
 
 
 # ------ test realm ------
